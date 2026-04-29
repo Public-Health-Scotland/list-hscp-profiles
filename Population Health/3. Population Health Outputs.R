02b916c264a9ce8cd67d57129ae3cc58372a1a1b
cfraser2020 <craig.fraser3@phs.scot>
2026-04-29 16:39:52 +0000

    name

--- Population Health/3. Population Health Outputs.R
+++ Population Health/3. Population Health Outputs.R
@@ -960,7 +960,7 @@ title <- ggdraw() +
   draw_label(
     str_wrap(
       glue(
-        "Prevalence of Physical Long-Term Conditions {latest_year_ltc} in the {HSCP} HSCP"
+        "Percentage Estimate of Physical Long-Term Conditions {latest_year_ltc} in the {HSCP} HSCP"
       ),
       width = 65
     ),
